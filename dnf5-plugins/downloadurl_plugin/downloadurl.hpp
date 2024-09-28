38c3f3558a934261db5a6ac93a2c01e3f3f333ad
Ali Mirjamali <ali@mirjamali.com>
2024-09-28 22:07:49 +0330

    Add `downloadurl` plugin for DNF5
    reference: https://github.com/QubesOS/qubes-core-agent-linux/pull/522

---
+++ dnf5-plugins/downloadurl_plugin/downloadurl.hpp
@@ -0,0 +1,60 @@
+/*
+Copyright Contributors to the libdnf project.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef DNF5_COMMANDS_DOWNLOADURL_DOWNLOAD_HPP
+#define DNF5_COMMANDS_DOWNLOADURL_DOWNLOAD_HPP
+
+#include <dnf5/context.hpp>
+#include <libdnf5/conf/option.hpp>
+
+#include <memory>
+#include <set>
+#include <vector>
+
+
+namespace dnf5 {
+
+
+class DownloadURLCommand : public Command {
+public:
+    explicit DownloadURLCommand(Context & context) : Command(context, "downloadurl") {}
+    void set_parent_command() override;
+    void set_argument_parser() override;
+    void configure() override;
+    void run() override;
+
+private:
+    std::set<std::string> urlprotocol_valid_options;
+    std::set<std::string> urlprotocol_option;
+    std::set<std::string> arch_option;
+    libdnf5::OptionBool * resolve_option{nullptr};
+    libdnf5::OptionBool * alldeps_option{nullptr};
+    libdnf5::OptionBool * url_option{nullptr};
+    libdnf5::OptionBool * urlallmirrors_option{nullptr};
+    libdnf5::OptionBool * srpm_option{nullptr};
+
+    std::vector<std::unique_ptr<libdnf5::Option>> * patterns_to_download_options{nullptr};
+};
+
+
+}  // namespace dnf5
+
+
+#endif  // DNF5_COMMANDS_DOWNLOADURL_DOWNLOAD_HPP
